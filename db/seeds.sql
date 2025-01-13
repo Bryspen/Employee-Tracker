05b427c78bb80badfe9bdf904151518ed290b583
Bryan Spencer <bryanmspencer@gmail.com>
2025-01-13 16:30:26 -0600

    updated query and server files. Also readme.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,25 @@
 INSERT INTO department (name)
-VALUES  ('Marketing');
-        ('Legal');
-        ('Accounting');
-        ('Product Management');
+VALUES  ('Marketing'),
+        ('Legal'),
+        ('Accounting'),
+        ('Product Management'),
         ('Human Resources');
 
 INSERT INTO role (title, salary, department_id)
-VALUES  ('Director of Marketing', 89984, 1);
-        ('Community Outreach Specialist', 57572, 1);
-        ('Assistant Paralegal', 48805, 2);
-        ('Cost Accountant', 56196, 3);
-        ('Chief Design Engineer', 52170, 4);
-        ('Developer III', 34837, 4);
-        ('HR Representative', 33457, 5);
+VALUES  ('Director of Marketing', 89984, 1),
+        ('Community Outreach Specialist', 57572, 1),
+        ('Assistant Paralegal', 48805, 2),
+        ('Cost Accountant', 56196, 3),
+        ('Chief Design Engineer', 52170, 4),
+        ('Developer III', 34837, 4),
+        ('HR Representative', 33457, 5),
         ('HR Director', 57138, 5);
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ('Gardy', 'Tomczynski', 6,null);
-('Alyssa', 'Mathiassen', 8, null);
-('Cyril', 'Chamberlin', 2, 4);
-('Neale', 'Pasmore', 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Gardy', 'Tomczynski', 6,null),
+('Alyssa', 'Mathiassen', 8, null),
+('Cyril', 'Chamberlin', 2, 4),
+('Neale', 'Pasmore', 1, null),
 ('Elianore', 'Begwell', 7, 2);
 
 
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import express from 'express';
-import { QueryResult } from 'pg';
-import { pool, connectToDb } from './connection.js';
-import inquirer from 'inquirer'
-import {viewAllDepartments,
-        viewAllRoles,
-        addDepartment,
-        addRole,
-        addEmployee,
-        updateEmployeeRole,
-        quit} from './queries.js'
+import { connectToDb } from './connection.js';
+import inquirer from 'inquirer';
+import {
+    viewAllDepartments,
+    viewAllRoles,
+    viewAllEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployeeRole,
+    appQuit,
+} from './queries.js';
 
 await connectToDb();
 
@@ -19,44 +21,71 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//App Start
-function appStart () {
-    inquirer
-    .prompt ([
+const mainMenu = async () => {
+    const choices = [
+        'View All Departments',
+        'View All Roles',
+        'View All Employees',
+        'Add Department',
+        'Add Role',
+        'Add Employee',
+        'Update Employee Role',
+        'Quit'
+    ];
+
+    const { action } = await inquirer.prompt([
         {
-        type:'list',
-        name:'prompt',
-        message: 'What would you like to do?',
-                            choices: ['View All Departments',
-                                     'View All Roles',
-                                     'View All Employees',
-                                     'Add a Department',
-                                     'Add a Role',
-                                     'Add an Employee',
-                                     'Update an Employee Role',
-                                     'Quit']
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices
         }
-    ])
-    .then ((answers) => {
-        if (answers.prompt === 'View All Departments'){
-            viewAllDepartments();
-        }
-        else if (answers.prompt === 'View All Roles'){
-            viewAllRoles();
-        }
+    ]);
 
+    switch (action) {
+        case 'View All Departments':
+            await viewAllDepartments();
+            break;
+        case 'View All Roles':
+            await viewAllRoles();
+            break;
+        case 'View All Employees':
+            await viewAllEmployees();
+            break;
+        case 'Add Department':
+            await addDepartment();
+            break;
+        case 'Add Role':
+            await addRole();
+            break;
+        case 'Add Employee':
+            await addEmployee();
+            break;
+        case 'Update Employee Role':
+            await updateEmployeeRole();
+            break;
+        case 'Quit':
+            await appQuit();
+            return;
     }
 
+    // Loop back to the main menu
+    await mainMenu();
+};
 
-    }
+const appStart = async () => {
+    console.log('Welcome to the Employee Management System');
+    await mainMenu();
+};
 
-    appStart()
-// app.use((_req, res) => {
-//   res.status(404).end();
-// });
-//
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+await appStart();
 
-export {appStart}
+app.use((_req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+export { appStart };
